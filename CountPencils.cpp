8620dd59af113ab12884f06e4cd681ae619eede6
Sergio Can <gersiopumas@gmail.com>
2017-12-15 23:34:25 -0500

    Added file.

---
+++ CountPencils.cpp
@@ -0,0 +1,25 @@
+/*
+* CountPencil.cpp
+* Author: Sergio Can
+* Description:
+*   Read a N number, Read a K number
+*   Read N lines of numbers and print the times that K appeared in the N lines.
+* Example case: 5 1
+* 1 3 4 2 1 -> 2
+*/
+#include<iostream>
+
+int main(){
+  int N,k,aux;
+  std::cin>>N;
+  std::cin>>k;
+
+  int count = 0;
+  for(int i = 0;i < N;i++){
+    std::cin>> aux;
+    if(aux == k)
+      count++;
+  }
+  std::cout << count;
+  return 0;
+}
